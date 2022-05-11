88fb1268a2174316ec7009d33ad4e4cc583cbeec
thomasop <tdss33@hotmail.com>
2022-05-11 13:38:39 +0200

    Add: Product Entity and test #6

---
+++ tests/Entity/ProductTest.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+namespace App\Tests\Entity;
+
+use App\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+final class ProductTest extends KernelTestCase
+{
+    public function test(): void
+    {
+        $product = (new Product())
+        ->setPrice(10)
+        ->setTitle("title")
+        ->setImage("image.jpg");
+        self::bootKernel();
+        $error = self::getContainer()->get('validator')->validate($product);
+        $this->assertCount(0, $error);
+    }
+
+    public function testPrice(): void
+    {
+        $product = new Product();
+        $price = 10;
+
+        $product->setPrice($price);
+        $this->assertEquals(10, $product->getPrice());
+    }
+
+    public function testTitle(): void
+    {
+        $product = new Product();
+        $title = "title";
+
+        $product->setTitle($title);
+        $this->assertEquals("title", $product->getTitle());
+    }
+
+    public function testImage(): void
+    {
+        $product = new Product();
+        $image = "image.jpg";
+
+        $product->setImage($image);
+        $this->assertEquals("image.jpg", $product->getImage());
+    }
+}

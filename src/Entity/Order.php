fb233a3b3af61a33b834049a0e3b7ef78dae7fa9
thomasop <tdss33@hotmail.com>
2022-05-11 13:35:14 +0200

    Add: Order Entity and test #7

---
+++ src/Entity/Order.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\User;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\OrderRepository;
+
+#[ORM\Entity(repositoryClass: OrderRepository::class)]
+#[ORM\Table(name: '`order`')]
+class Order
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $product;
+
+    #[ORM\Column(type: 'float')]
+    private $price;
+
+    #[ORM\Column(type: 'integer')]
+    private $quantity;
+
+    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'orders')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $user;
+
+    #[ORM\Column(type: 'boolean')]
+    private $status;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $date;
+
+    public function __construct()
+    {
+        $this->status = true;
+    }
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProduct(): ?string
+    {
+        return $this->product;
+    }
+
+    public function setProduct(string $product): self
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(bool $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(?\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
---
+++ tests/Entity/OrderTest.php
@@ -0,0 +1,68 @@
+<?php declare(strict_types=1);
+
+namespace App\Tests\Entity;
+
+use App\Entity\Order;
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+final class OrderTest extends KernelTestCase
+{
+    public function test(): void
+    {
+        $order = (new Order())
+        ->setProduct("Test")
+        ->setPrice(10)
+        ->setQuantity(2)
+        ->setDate(new DateTime('now'))
+        ->setStatus(true);
+        self::bootKernel();
+        $error = self::getContainer()->get('validator')->validate($order);
+        $this->assertCount(0, $error);
+    }
+
+    public function testProduct(): void
+    {
+        $order = new Order();
+        $product = "test";
+
+        $order->setProduct($product);
+        $this->assertEquals("test", $order->getProduct());
+    }
+
+    public function testQuantity(): void
+    {
+        $order = new Order();
+        $quantity = 1;
+
+        $order->setQuantity($quantity);
+        $this->assertEquals(1, $order->getQuantity());
+    }
+
+    public function testDate(): void
+    {
+        $order = new Order();
+        $date = new DateTime('2011-01-01T15:03:01.012345Z');
+
+        $order->setDate($date);
+        $this->assertEquals(new DateTime('2011-01-01T15:03:01.012345Z'), $order->getDate());
+    }
+
+    public function testStatus(): void
+    {
+        $order = new Order();
+        $status = true;
+
+        $order->setStatus($status);
+        $this->assertEquals(true, $order->getStatus());
+    }
+
+    public function testPrice(): void
+    {
+        $order = new Order();
+        $price = 10;
+
+        $order->setPrice($price);
+        $this->assertEquals(10, $order->getPrice());
+    }
+}
